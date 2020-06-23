208114a4b71875d16b0635236600f1654f67e88d
oluwatobi1 <akintunlesetobi@gmail.com>
2020-06-23 11:21:51 +0100

    Code improvement

--- JS/tictactoe.js
+++ JS/tictactoe.js
@@ -1,33 +1,25 @@
-var oneblock = document.querySelector('#one')
-var twoblock = document.querySelector('#two')
-var threeblock = document.querySelector('#three')
-var fourblock = document.querySelector('#four')
-var fiveblock = document.querySelector('#five')
-var sixblock = document.querySelector('#six')
-var sevenblock = document.querySelector('#seven')
-var eightblock = document.querySelector('#eight')
-var nineblock = document.querySelector('#nine')
+var restart = document.querySelector("#restart")
 
-function swap(item) {
-  if (item.textContent === ""){
-    item.textContent = "X"
-  }else if (item.textContent === "X") {
-    item.textContent = "O"
-  }else if (item.textContent === "O") {
-    item.textContent = null
-  }else {
-    console.log("something weird");
+var tiles = document.querySelectorAll("td")
+
+function clearall() {
+  for (var i = 0; i < tiles.length; i++) {
+    tiles[i].textContent = ""
   }
 }
+restart.addEventListener("click", clearall)
 
-oneblock.addEventListener("click", function(){
-  swap(oneblock);
-});
-twoblock.addEventListener("click", function(){swap(twoblock);});
-threeblock.addEventListener("click", function(){swap(threeblock);});
-fourblock.addEventListener("click", function(){swap(fourblock);});
-fiveblock.addEventListener("click", function(){swap(fiveblock);});
-sixblock.addEventListener("click", function(){swap(sixblock);});
-sevenblock.addEventListener("click", function(){swap(sevenblock);});
-eightblock.addEventListener("click", function(){swap(eightblock);});
-nineblock.addEventListener("click", function(){swap(nineblock);});
+function change() {
+  if (this.textContent === "") {
+    this.textContent = "X"
+  }else if (this.textContent === "X") {
+    this.textContent = "O"
+  }else{
+    this.textContent === "";
+  };
+}
+
+
+for (var i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener('click', change)
+}
